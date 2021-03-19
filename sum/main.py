0ba1888d8b67ab0c79f4506b5e99eef7264b8eed
Karthik Raja <67165351+karthiks233@users.noreply.github.com>
2021-03-19 22:19:56 +0530

    Update main.py

--- sum/main.py
+++ sum/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,request
-
+import requests
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
@@ -8,6 +8,22 @@ app = Flask(__name__)
 
 @app.route('/sum4',methods=['POST'])
 def a():
+    import requests
+
+    url = 'https://input-json-data-dot-folk-dev-com-db.uc.r.appspot.com/json-example'
+    data1 = {
+        "language" : "Python",
+        "framework" : "Flask",
+        "website" : "Scotch",
+        "version_info" : {
+            "python" : "3.9.0",
+            "flask" : "1.1.2"
+        },
+        "examples" : ["query", "form", "json"],
+        "boolean_test" : True}
+
+    resp=requests.post(url,json=data1)
+    return resp.text
     n1=request.form['num1']
     n2=request.form['num2']
     x=float(n1)
